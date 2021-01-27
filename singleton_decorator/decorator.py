e8e239d00eeb26ed127b68aa9650ce34dfef2c72
vtpl1 <monotosh.das@videonetics.com>
2021-01-27 16:42:30 +0530

    Update decorator.py

--- singleton_decorator/decorator.py
+++ singleton_decorator/decorator.py
@@ -1,3 +1,5 @@
+from threading import Lock
+__lock = Lock()
 class _SingletonWrapper:
     """
     A singleton wrapper class. Its instances would be created
@@ -6,29 +8,32 @@ class _SingletonWrapper:
     def __init__(self, cls):
         self.__wrapped__ = cls
         self._instance = None
-        self._instance_dict = None
-        self._is_instance_dict = False
+        with __lock:
+            self._instance_dict = None
+            self._is_instance_dict = False
         a = dir(cls)
         # print("*******", a)
         if 'key' in a:
             print('++++++++++++++++ key found {} {}'.format(cls, type(a)))
-            self._instance_dict = {}
-            self._is_instance_dict = True
+            with __lock:
+                self._instance_dict = {}
+                self._is_instance_dict = True
 
     def __call__(self, *args, **kwargs):
         """Returns a single instance of decorated class"""
-        if self._is_instance_dict is True:
-            temp = self.__wrapped__(*args, **kwargs)
-            key = temp.key
-            if key in self._instance_dict:
-                temp = self._instance_dict[key]
+        with __lock:
+            if self._is_instance_dict is True:
+                temp = self.__wrapped__(*args, **kwargs)
+                key = temp.key
+                if key in self._instance_dict:
+                    temp = self._instance_dict[key]
+                else:
+                    self._instance_dict[key] = temp
+                return temp
             else:
-                self._instance_dict[key] = temp
-            return temp
-        else:
-            if self._instance is None:
-                self._instance = self.__wrapped__(*args, **kwargs)
-            return self._instance
+                if self._instance is None:
+                    self._instance = self.__wrapped__(*args, **kwargs)
+                return self._instance
 
 
 def singleton(cls):
