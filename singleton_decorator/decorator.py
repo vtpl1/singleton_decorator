5f62d0353d7f7c1467c1b9b18c62d5b756162221
Monotosh Das <monotosh.das@videonetics.com>
2021-01-28 04:24:00 +0000

    lock added

--- singleton_decorator/decorator.py
+++ singleton_decorator/decorator.py
@@ -1,35 +1,52 @@
+import threading
+import logging
+
+LOGGER = logging.getLogger("root")
+
+
 class _SingletonWrapper:
     """
     A singleton wrapper class. Its instances would be created
     for each decorated class.
     """
+    lock = threading.Lock()
+
     def __init__(self, cls):
         self.__wrapped__ = cls
         self._instance = None
         self._instance_dict = None
         self._is_instance_dict = False
         a = dir(cls)
-        print("*******", a)
-        if "key" in a:
-            print("++++++++++++++++ key found {} {}".format(cls, type(a)))
-            print("Subhasis")
-            self._instance_dict = {}
-            self._is_instance_dict = True
+        LOGGER.info(f"++++++++++++++ SingletonWrapper init called {a}")
+        with _SingletonWrapper.lock:
+            if "key" in a:
+                LOGGER.info(
+                    f"++++++++++++++ SingletonWrapper key found {cls} {type(a)}"
+                )
+                self._instance_dict = {}
+                self._is_instance_dict = True
 
     def __call__(self, *args, **kwargs):
         """Returns a single instance of decorated class"""
-        if self._is_instance_dict is True:
-            temp = self.__wrapped__(*args, **kwargs)
-            key = temp.key
-            if key in self._instance_dict:
-                temp = self._instance_dict[key]
+        with _SingletonWrapper.lock:
+            if self._is_instance_dict is True:
+                temp = self.__wrapped__(*args, **kwargs)
+                key = temp.key
+                if key in self._instance_dict:
+                    temp = self._instance_dict[key]
+                    LOGGER.info(
+                        f"++++++++++++++ SingletonWrapper returning existing instance"
+                    )
+                else:
+                    self._instance_dict[key] = temp
+                    LOGGER.info(
+                        f"++++++++++++++ SingletonWrapper returning new instance"
+                    )
+                return temp
             else:
-                self._instance_dict[key] = temp
-            return temp
-        else:
-            if self._instance is None:
-                self._instance = self.__wrapped__(*args, **kwargs)
-            return self._instance
+                if self._instance is None:
+                    self._instance = self.__wrapped__(*args, **kwargs)
+                return self._instance
 
 
 def singleton(cls):
